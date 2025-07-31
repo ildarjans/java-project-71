33d90ed5d02b2bce17462a80276f8d73ede97141
ildarjans <nobap84@rambler.ru>
2025-08-01 00:40:16 +0300

    application entry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
+    application
 }
 
 group = "hexlet.code"
@@ -8,6 +9,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 dependencies {
