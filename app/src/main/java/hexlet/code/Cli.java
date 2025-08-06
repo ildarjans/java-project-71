1a5994eec0112222593e2b96ee337d2b6399e81a
ildarjans <nobap84@rambler.ru>
2025-08-07 00:15:49 +0300

    add format and params

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,12 +1,23 @@
 package hexlet.code;
 
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "0.0.1",
         description = "Compares two configuration files and shows a difference.")
 public class Cli implements Callable<Integer> {
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", hideParamSyntax = true)
+    private String format = "stylish";
+
     @Override
     public Integer call() {
         return 0;
