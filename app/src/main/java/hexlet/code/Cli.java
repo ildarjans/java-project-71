4ba2e27738c4f8e10ee0350e983af8eaadb9b902
ildarjans <nobap84@rambler.ru>
2025-08-07 01:23:07 +0300

    use jackson for parse files

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,9 +1,15 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "0.0.1",
@@ -19,7 +25,21 @@ public class Cli implements Callable<Integer> {
     private String format = "stylish";
 
     @Override
-    public Integer call() {
+    public Integer call() throws Exception {
+        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        if (!Files.exists(path1) || !Files.exists(path2)) {
+            throw new Exception("File does not exist");
+        }
+
+
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map1 = objectMapper.readValue(content1, new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> map2 = objectMapper.readValue(content2, new TypeReference<Map<String,Object>>(){});
+
         return 0;
     }
 }
